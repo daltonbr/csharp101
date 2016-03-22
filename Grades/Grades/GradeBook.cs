d8212c876e528bed720669feaf19f477b9f20117
daltonbr <sacinopatinete@gmail.com>
2016-03-22 16:07:04 -0300

    Finish 'Classes and Object' module

---
+++ Grades/Grades/GradeBook.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grades
+{
+    class GradeBook
+    {
+        public static float MinimumGrade = 0;
+        public static float MaximumGrade = 100;
+        List<float> grades = new List<float>();
+        
+        public GradeBook()
+        {
+            grades = new List<float>();
+        }
+
+        public GradeStatistics ComputeStatistics()
+        {
+            GradeStatistics stats = new GradeStatistics();
+            
+            //average grade
+            float sum = 0;
+            foreach(float grade in grades)
+            {
+                sum += grade;
+                
+                //if ( grade > stats.HighestGrade )
+                //{
+                //    stats.HighestGrade = grade;
+                //}
+
+                //if (grade < stats.LowestGrade)
+                //{
+                //    stats.LowestGrade = grade;
+                //}
+
+                // this two lines are equivalent to the 2 IF's above - so elegant =)
+                stats.HighestGrade = Math.Max(grade, stats.HighestGrade);
+                stats.LowestGrade = Math.Min(grade, stats.LowestGrade);               
+            }
+            stats.AverageGrade = sum / grades.Count; // beware the division by zero when grades has zero elements
+
+            return stats;
+        } 
+
+
+    public void AddGrade(float _grade)
+        {
+            grades.Add(_grade);
+           
+        }
+        
+
+    }
+}
---
+++ Grades/Grades/GradeStatistics.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grades
+{
+    class GradeStatistics
+    {
+        // constructor
+        public GradeStatistics()
+        {
+            this.HighestGrade = 0;
+            this.LowestGrade = float.MaxValue;
+        }
+
+        // variables
+        public float AverageGrade;
+        public float HighestGrade;
+        public float LowestGrade;
+    }
+}
